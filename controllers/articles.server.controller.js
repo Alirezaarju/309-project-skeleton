3d29ebc5aa06eec36035cd66d595f0cc79081180
Ali Reza Arju <alirezaarju@gmail.com>
2018-03-14 03:14:25 +0000

    Assignment-10

--- controllers/articles.server.controller.js
+++ controllers/articles.server.controller.js
@@ -3,6 +3,44 @@ var Article = require('./../models/Article.js');
 var errorHandler = require('./errors.server.controller');
 var _ = require('lodash');
 
+module.exports.createView = function(req, res){
+  res.render('./../public/views/article/new.ejs', {
+          user: req.user || null,
+          request: req
+        });
+};
+
+module.exports.singleView = function(req, res){
+  res.render('./../public/views/article/view.ejs', {
+          user: req.user || null,
+          request: req
+        });
+}
+
+module.exports.listView = function(req, res) {
+    Article.find(function(err, data) {
+      if (err) {
+        return res.status(400).send({
+
+          message: errorHandler.getErrorMessage(err)
+        });
+      }
+      else {
+        console.log("api called");
+
+        res.render('./../public/views/article/all.ejs', {
+          user: req.user || null,
+          request: req,
+          articles: data
+        });
+      }
+    });
+  
+	
+};
+
+
+
 module.exports.list = function(req, res) {
   Article.find(function(err, data) {
     if (err) {
@@ -14,39 +52,9 @@ module.exports.list = function(req, res) {
       console.log("api called");
 
       res.status(200).send(data);
-      
     }
   });
 };
-module.exports.listView= function(req, res){
-    Article.find(function(err, data) {
-    if (err) {
-      return res.status(400).send({
-
-  				message: errorHandler.getErrorMessage(err)
-  			});
-    } else {
-      console.log("api called");
-      
-  res.render('./../public/views/article/all.ejs',{
-    user: req.user || null,
-    request: req,
-    articles: data
-
-  });
-    }
-});
-  
-};
-
-  module.exports.SingleView = function(req, res){
-      res.render('./../public/views/article/view.ejs',{
-    user: req.user || null,
-    request: req
-
-
-  });
-  };
 
 module.exports.create = function(req, res) {
   var article = new Article(req.body);
@@ -102,4 +110,3 @@ exports.articleByID = function(req, res, next, id) {
 		next();
 	});
 };
-
